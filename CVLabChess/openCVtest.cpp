67bffe9f10d3ffe36479c468f79787cb000f3a62
Dario Castro <dariocastro149@googlemail.com>
2021-12-09 20:32:33 +0100

    added way to find all spots where figures can be placed

--- CVLabChess/openCVtest.cpp
+++ CVLabChess/openCVtest.cpp
@@ -9,13 +9,15 @@ using namespace std;
 
 //ID depending on system, default is 0
 #define WEBCAM_ID 1
+#define EPSILON 0.001
+const int EPSILON_LINES = 10;
 
 const string WINDOW = "Setting up the Board";
 const float WIDTH = 500;
 const float HEIGHT = 500;
 
-int threshMinCanny = 50;
-int threshMaxCanny = 145;
+int threshMinCanny = 10;
+int threshMaxCanny = 100;
 int threshMinArea = 1000;
 int threshMaxArea = 2000;
 
@@ -28,7 +30,12 @@ int sliderMaxArea = 2000;
 void recognizeBoard(Mat img);
 Mat cannyBoard(Mat img);
 Mat contourBoard(Mat img);
-Mat houghBoard(Mat img);
+void drawLines(Mat img, vector<vector<Point>>& horizontalLines, vector<vector<Point>>& verticalLines);
+void getIntersections(const vector<vector<Point>> horizontalLines, const vector<vector<Point>> verticalLines, vector<Point>& intersections);
+void drawIntersections(Mat img, const vector<Point> intersections);
+void getBoardFields(vector<Point> intersections, vector<Point>& boardFields);
+void getHoughLines(const Mat img, vector<vector<Point>>& horizontalLines, vector<vector<Point>>& verticalLines);
+void sortLines(const vector<vector<Point>> src, vector<vector<Point>>& dst);
 Mat warpBoard(Mat img, vector<Point> points, float width, float height);
 vector<Point> getMaxRect(Mat img);
 void drawRect(Mat img, vector<Point> points);
@@ -57,19 +64,31 @@ int main() {
 
 	VideoCapture cap(WEBCAM_ID);
 	namedWindow(WINDOW);
-	Mat img, img_scanned, img_static_resized, img_static_warped;
+	Mat img, img_scanned, img_static_resized, img_static_warped, img_static_cannyed, img_static_houghed, img_static_intersected;
+	vector<vector<Point>> horizontalLines, verticalLines;
+	vector<Point> intersections, boardFields;
 
 	string path = "Ressources/chessboard2.png";
 	Mat img_board = imread(path);
 	resize(img_board, img_static_resized, { 600 , 400 });
 	vector<Point> maxRect = getMaxRect(img_static_resized);
 	img_static_warped = warpBoard(img_static_resized, maxRect, 500, 500);
+	img_static_cannyed = cannyBoard(img_static_warped);
+	getHoughLines(img_static_cannyed, horizontalLines, verticalLines);
+	drawLines(img_static_warped, horizontalLines, verticalLines);
+	getIntersections(horizontalLines, verticalLines, intersections);
+	//drawIntersections(img_static_warped, intersections);
+	getBoardFields(intersections, boardFields);
+	drawIntersections(img_static_warped, boardFields);
+
 
 	drawRect(img_static_resized, maxRect);
 	imshow("static keypoints", img_static_resized);
+	//imshow("static cannyed", img_static_cannyed);
 	imshow("static warped", img_static_warped);
+	//imshow("static houghed", img_static_houghed);
 
-	
+
 
 	createTrackbar("Min Canny", WINDOW, &sliderMinCanny, 250, on_trackbar_min_canny);
 	createTrackbar("Max Canny", WINDOW, &sliderMaxCanny, 250, on_trackbar_max_canny);
@@ -83,7 +102,7 @@ int main() {
 		imshow(WINDOW, img_scanned);
 		waitKey(1);
 	}
-	
+
 	return 0;
 }
 
@@ -144,6 +163,57 @@ void drawRect(Mat img, vector<Point> points) {
 	for (int i = 0; i < points.size(); i++) {
 		circle(img, points[i], 5, red, FILLED);
 		putText(img, to_string(i), points[i], FONT_HERSHEY_PLAIN, 2, red, 2);
+	}
+}
+
+void drawLines(Mat img, vector<vector<Point>>& horizontalLines, vector<vector<Point>>& verticalLines) {
+
+	for (int i = 0; i < horizontalLines.size(); i++) {
+		line(img, horizontalLines[i][0], horizontalLines[i][1], Scalar(0, 255, 0), 1);
+	}
+
+	for (int i = 0; i < verticalLines.size(); i++) {
+		line(img, verticalLines[i][0], verticalLines[i][1], Scalar(0, 255, 0), 1);
+	}
+}
+
+void getIntersections(const vector<vector<Point>> horizontalLines, const vector<vector<Point>> verticalLines, vector<Point>& intersections) {
+	intersections = {};
+	for (int vertical = 0; vertical < verticalLines.size(); vertical++) {
+		for (int horizontal = 0; horizontal < horizontalLines.size(); horizontal++) {
+			int x = horizontalLines[horizontal][0].x;
+			int y = verticalLines[vertical][1].y;
+			intersections.push_back({ x,y });
+		}
+	}
+}
+
+void drawIntersections(Mat img, const vector<Point> intersections) {
+	Scalar red = Scalar(0, 0, 255);
+
+	for (int i = 0; i < intersections.size(); i++) {
+		cout << "Intersection " << i << ": " << intersections[i] << endl;
+		circle(img, intersections[i], 5, red, FILLED);
+		putText(img, to_string(i), intersections[i], FONT_HERSHEY_PLAIN, 1, red, 1);
+	}
+}
+
+void getBoardFields(vector<Point> intersections, vector<Point>& boardFields) {
+	int offset = sqrt(intersections.size()) + 1;
+	Point topLeftPoint;
+	Point bottomRightPoint;
+	boardFields = {};
+	int counter = 0;
+	for (int i = 0; i < intersections.size() - offset; i++) {
+		if (counter < offset - 2) {
+			topLeftPoint = intersections[i];
+			bottomRightPoint = intersections[i + offset];
+			boardFields.push_back({ topLeftPoint.x + cvRound((bottomRightPoint.x - topLeftPoint.x) / 2), topLeftPoint.y + cvRound((bottomRightPoint.y - topLeftPoint.y) / 2) });
+			counter++;
+		}
+		else {
+			counter = 0;
+		}
 	}
 }
 
@@ -209,23 +279,73 @@ Mat contourBoard(const Mat img) {
 	return contoured;
 }
 
-Mat houghBoard(const Mat img) {
+void getHoughLines(const Mat img, vector<vector<Point>> &horizontalLines, vector<vector<Point>> &verticalLines) {
 	vector<Vec2f> lines;
+	horizontalLines = {};
+	verticalLines = {};
 
 	HoughLines(img, lines, 1, CV_PI / 180, 120, 0, 0);
 
 	Mat houghed(img.size(), CV_8UC1, Scalar(0, 0, 0));
+
 	for (size_t i = 0; i < lines.size(); i++) {
 		float rho = lines[i][0], theta = lines[i][1];
-		Point pt1, pt2;
-		double a = cos(theta), b = sin(theta);
-		double x0 = a * rho, y0 = b * rho;
-		pt1.x = cvRound(x0 + 1000 * (-b));
-		pt1.y = cvRound(y0 + 1000 * (a));
-		pt2.x = cvRound(x0 - 1000 * (-b));
-		pt2.y = cvRound(y0 - 1000 * (a));
-		line(houghed, pt1, pt2, Scalar(255, 255, 255), 1);
+		cout << "Rho:" << rho << endl;
+		cout << "Theta:" << theta << endl;
+		cout << "PI/2:" << CV_PI / 2 << endl;
+
+		// check which houghLines have 0° or 180° angle
+		if (theta < EPSILON) {
+			Point pt1, pt2;
+			double a = cos(theta), b = sin(theta);
+			double x0 = a * rho, y0 = b * rho;
+			pt1.x = cvRound(x0 + 1000 * (-b));
+			pt1.y = 0;
+			pt2.x = pt1.x;
+			pt2.y = WIDTH;
+			cout << "Horizontal:" << pt1 << "," << pt2 << endl;
+			horizontalLines.push_back({ pt1, pt2 });
+		}
+		else if (theta < EPSILON || abs(theta - CV_PI / 2) < EPSILON) {
+			Point pt1, pt2;
+			double a = cos(theta), b = sin(theta);
+			double x0 = a * rho, y0 = b * rho;
+			pt1.x = 0;
+			pt1.y = cvRound(y0 + 1000 * (a));
+			pt2.x = HEIGHT;
+			pt2.y = pt1.y;
+			cout << "Vertical:" << pt1 << "," << pt2 << endl;
+			verticalLines.push_back({ pt1, pt2 });
+		}
 	}
 
-	return houghed;
+	//sort lines
+	sort(horizontalLines.begin(), horizontalLines.end(), [](const vector<Point> a, const vector<Point> b) { return a[0].x < b[0].x; });
+	sort(verticalLines.begin(), verticalLines.end(), [](const vector<Point> a, const vector<Point> b) { return a[0].y < b[0].y; });
+
+	//remove close horizontal lines
+	if (horizontalLines[0][0].x < EPSILON_LINES) {
+		horizontalLines.erase(horizontalLines.begin());
+	}
+	if ((WIDTH - horizontalLines[horizontalLines.size() - 1][0].x) < EPSILON_LINES) {
+		horizontalLines.pop_back();
+	}
+	for (int i = 0; i < horizontalLines.size() - 1; i++) {
+		if ((horizontalLines[i + 1][0].x - horizontalLines[i][0].x) < EPSILON_LINES) {
+			horizontalLines.erase(horizontalLines.begin() + i);
+		}
+	}
+
+	//remove close vertical lines
+	if (verticalLines[0][0].y < EPSILON_LINES) {
+		verticalLines.erase(verticalLines.begin());
+	}
+	if ((HEIGHT - verticalLines[horizontalLines.size() - 1][0].y) < EPSILON_LINES) {
+		verticalLines.pop_back();
+	}
+	for (int i = 0; i < verticalLines.size() - 1; i++) {
+		if ((verticalLines[i + 1][0].y - verticalLines[i][0].y) < EPSILON_LINES) {
+			verticalLines.erase(verticalLines.begin() + i);
+		}
+	}
 }
